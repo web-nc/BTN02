e432f772291d88f0dc32298bfecd29971d7cb263
black055 <vgia1055@gmail.com>
2021-12-14 22:55:46 +0700

    update grade

--- api/components/grade/grade.controller.js
+++ api/components/grade/grade.controller.js
@@ -1,23 +1,40 @@
 import Assignment from "../assignment/assignment.model.js";
+import Course from "../course/course.model.js";
 import Grade from "./grade.model.js";
 
 export default {
-    getGrades: (req, res) => {
+    getGrades: async (req, res) => {
         const courseId = req.params.courseId;
-        const studentId = req.query.studentId;
-        
-        Assignment.find({course: courseId}, "id")
-        .sort('order')
+        const course = await Course.findById(courseId, "gradeBoard");
+
+        Assignment.find({
+            course: courseId,
+        }, "id")
         .exec(async (e, assignments) => {
             if (e) {
                 return res.status(500).json({ message: e });
             }
-            const result = await Grade.find({
+            let result = await Grade.find({
                 assignment: { $in: assignments },
-                studentId: studentId
+                studentId: { $in: course.gradeBoard.map(el => el.studentId) }
             });
-            
             return res.status(200).json(result);
         });
+    },
+
+    editGrade: async (req, res) => {
+        const { assignment, studentId, point } = req.body.data;
+        
+        Grade.updateOne(
+            { assignment: assignment, studentId: studentId},
+            { point: point},
+            { upsert: true },
+            function (err, doc) {
+                if (err) return res.send(500, {error: err});
+                res.status(200).json({ message: "UPDATE_SUCCESSFUL" });
+            }
+        );
+
+        
     },
 };
--- app/src/components/CourseDetail/GradeBoard/index.js
+++ app/src/components/CourseDetail/GradeBoard/index.js
@@ -1,23 +1,40 @@
-/* eslint-disable no-loop-func */
-import { Paper } from "@mui/material";
+import { Paper, Card, CardHeader, CardContent } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
-import { getGrades } from "../../../services/grade";
+import { getGrades, editGrade } from "../../../services/grade";
+import { toast } from "react-toastify";
 
 export default function GradeBoard({ course, assignments }) {
   const paperStyle = {
     width: "60%",
-    margin: "30px auto",
-    height: 300,
+    margin: "30px auto"
   };
 
-  const [rows, setRows] = useState(
-    course.gradeBoard ?
-    (course.gradeBoard.map((student) => ({
-      ...student,
-      id:student.studentId
-    }))) : []
-  );
+  const calcGPA = (row) => {
+    const totalAssignmentsWeight = assignments.reduce((pre, cur) => pre + cur.weight, 0);
+    let GPA = 0;
+      for (const property in row) {
+        if (property !== 'id' && property !== 'studentId' && property !== 'studentName') {
+          const assignment = assignments.find(obj => { return obj._id === property });
+          GPA = GPA + assignment.weight * row[property];
+        }
+      }
+    return GPA = Math.round(GPA / totalAssignmentsWeight);
+  }
+
+  const handleEditCell = React.useCallback(
+    async (params) => {
+      editGrade({
+        assignment: params.field,
+        studentId: params.id,
+        point: params.value
+      }).catch(err => {
+        toast.error('Có lỗi xảy ra khi cập nhật!');
+      })
+    }
+  , []);
+
+  const [rows, setRows] = useState([]);
 
   const columns = [
           { field: 'studentId', headerName: 'MSSV' },
@@ -28,6 +45,14 @@ export default function GradeBoard({ course, assignments }) {
     columns.push({
       field: assignment._id,
       headerName: assignment.name,
+      editable: true,
+      preProcessEditCellProps: (params) => {
+        const hasError = isNaN(params.props.value) 
+                      || (params.props.value > 100)
+                      || (params.props.value < 0)
+                      || (params.props.value.length === 0);
+        return { ...params.props, error: hasError };
+      },
       renderCell: (params) => {
         if (params.value === undefined) {
           return '-';
@@ -36,11 +61,67 @@ export default function GradeBoard({ course, assignments }) {
       }
     });
   }
-  columns.push({ field: 'total', headerName: 'Điểm tổng kết' })
+  columns.push({
+    field: 'total',
+    headerName: 'Điểm tổng kết',
+    renderCell: (params) => {
+      return <strong>{calcGPA(params.row)}/100</strong>;
+    }
+  });
+
+  const handleUpdateRow = (id, assignment, point) => {
+    setRows((prevRows) => {
+      return prevRows.map((row, index) => {
+        if (row.id === id) {
+          let updatedRow = Object.assign({}, row);
+          updatedRow[assignment] = point;
+          return updatedRow;
+        }
+        else return row;
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (course.gradeBoard) {
+      setRows((course.gradeBoard.map((student) => ({
+        ...student,
+        id:student.studentId
+      }))));
+    }
+
+    course._id && getGrades(course._id).then(res => {
+      res.data.forEach((grade) => {
+        handleUpdateRow(grade.studentId, grade.assignment, grade.point);
+      });
+    })
+  }, [course._id, course.gradeBoard])
 
   return (
     <Paper elevation={10} style={paperStyle}>
-      <DataGrid rows={rows} columns={columns} disableColumnMenu autoHeight/>
+      <Card>
+        <CardHeader
+          sx={{ backgroundColor: "#f6f2f7", textAlign: "center" }}
+          title={
+            <strong>
+              [{course.briefName}] {course.name}
+            </strong>
+          }
+          subheader={"Người tạo lớp: " + (course.owner ? course.owner.name : "")}
+        />
+
+        <CardContent>
+          <DataGrid 
+            rows={rows}
+            columns={columns}
+            onSelectionModelChange={(newSelection, a) => {
+              console.log(newSelection, a);
+            }}
+            onCellEditCommit={handleEditCell}
+            disableColumnMenu
+            autoHeight/>
+        </CardContent>
+      </Card>
     </Paper>
   );
 }
